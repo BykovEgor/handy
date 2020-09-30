ccbdad3fee064ab173f5e5754ad443cb23a75c18
BykovEgor <48871249+BykovEgor@users.noreply.github.com>
2020-09-30 04:49:43 +0000

    3 window tmux session with htop and nvidia-smi

--- tmux-gpu-3pane.sh
+++ tmux-gpu-3pane.sh
@@ -2,7 +2,19 @@
 
 if [ -z $1 ]
 then
-    echo "No session name provided"
+    echo -n "Enter session name: "
+    read name
 else
     echo "Using session name $1"
 fi
+
+tmux new-session -d -s $1
+tmux rename-window -t 0 'main'
+tmux send-keys -t 'main' "ls -lah" C-m
+tmux split-window -t 'main' -v
+tmux send-keys "htop" C-m
+tmux split-window -h
+tmux send-keys "watch -n 1 -d nvidia-smi" C-m
+tmux resize-pane -R 23
+tmux selectp -t 0
+tmux attach-session -t $1
